e08aa0602a195aa8343b000ab4fade96549db79f
medkun <benkhaledmed11@gmail.com>
2018-11-16 13:48:34 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SliderComponent } from './slider/slider.component';
+import { CardsComponent } from './cards/cards.component';
+import { FormComponent } from './form/form.component';
+import { FooterComponent } from './footer/footer.component';
+import { CubeComponent } from './cube/cube.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SliderComponent,
+    CardsComponent,
+    FormComponent,
+    FooterComponent,
+    CubeComponent,
   ],
   imports: [
     BrowserModule,
